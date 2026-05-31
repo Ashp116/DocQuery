ac940d01c5ee919d3ab5bdcc64e23992a9c5ba95
Ashp116 <ashp116@outlook.com>
2026-05-31 19:22:25 -0400

    ADD: Model

---
+++ model.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+from transformers import Qwen2VLForConditionalGeneration, AutoProcessor, BitsAndBytesConfig
+from .encoder import DocVLMEncoder
+
+class DocVLM(nn.Module):
+    def __init__(self, qwen_model, encoder):
+        super().__init__()
+        self.qwen = self.qwen_model
+        self.encoder = encoder
+
+
+    def forward(self, ocr_inputs_ids, ocr_attention_mask, bbox, input_ids, attention_mask, labels=None, pixel_values=None, image_grid_thw=None):
+        ocr_tokens = self.encoder(
+            ocr_inputs_ids,
+            ocr_attention_mask,
+            bbox,
+        ).to(torch.float16)
+
+        text_embeds = self.qwen.model.embed_tokens(input_ids)
+
+        combined_embeds = torch.cat([ocr_tokens, text_embeds], dim=1)
+
+        B = input_ids.shape[0]
+        ocr_mask = torch.ones(B, 64, device=attention_mask.device)
+        combined_mask = torch.cat([ocr_mask, attention_mask], dim=1)
+
+        if labels is not None:
+            ignore = torch.full((B, 64), -100, device=labels.device)
+            labels = torch.cat([ignore, labels], dim=1)
+
+        outputs = self.qwen(
+            inputs_embeds=combined_embeds,
+            attention_mask=combined_mask,
+            pixel_values=pixel_values,
+            image_grid_thw=image_grid_thw,
+            labels=labels
+        )
+
+        return outputs
+    
+
+    @torch.no_grad()
+    def generate(self, ocr_input_ids, ocr_attention_mask, bbox, input_ids, attention_mask, **generate_kwargs):
+        ocr_tokens = self.encoder(
+            ocr_input_ids, ocr_attention_mask, bbox
+        ).to(torch.float16)
+
+        text_embeds = self.qwen.model.embed_tokens(input_ids)
+        combined_embeds = torch.cat([ocr_tokens, text_embeds], dim=1)
+
+        B = input_ids.shape[0]
+        ocr_mask = torch.ones(B, 64, device=attention_mask.device)
+        combined_masks = torch.cat([ocr_mask, attention_mask], dim=1)
+
+        return self.qwen.generate(
+            input_embeds=combined_embeds,
+            attention_mask=combined_masks,
+            **generate_kwargs
+        )
